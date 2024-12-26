d50d59335948e44dc81d427c707a5b1d10ecddd9
Martin Fauci <martinfauci@gmail.com>
2024-12-26 22:50:51 +0100

    completada carpeta security y varios DTOs

---
+++ src/main/java/com/alurachallenge/forohub/domain/curso/Categoria.java
@@ -0,0 +1,14 @@
+package com.alurachallenge.forohub.domain.curso;
+
+public enum Categoria {
+    SHONEN,
+    SHOUJO,
+    SEINEN,
+    JOSEI,
+    ISEKAI,
+    MECHA,
+    FANTASIA,
+    ACCION,
+    ROMANCE,
+    MISTERIO
+}
---
+++ src/main/java/com/alurachallenge/forohub/domain/usuario/AutenticacionUsuarioDTO.java
@@ -0,0 +1,4 @@
+package com.alurachallenge.forohub.domain.usuario;
+
+public record AutenticacionUsuarioDTO(String username, String password) {
+}
